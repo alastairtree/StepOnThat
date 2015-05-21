c863107ef6b33679434602f6e5f27dde5d1fe061
Alastair Crabtree <alastaircrabtree@gmail.com>
2015-05-21 23:55:38 +0100

    Add basic browser automation tooling and tests

---
+++ StepOnThat/Browser/Browser.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace StepOnThat.Browser
+{
+    public class Browser
+    {
+        private static Browser browser;
+
+        public static int DefailtWaitInSeconds = 5;
+
+        private IWebDriver driver;
+
+        public Browser()
+        {
+            driver = new ChromeDriver(new ChromeOptions());
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(DefailtWaitInSeconds));
+        }
+
+        public static Browser Current
+        {
+            get { return browser ?? (browser = new Browser()); }
+        }
+
+        public Browser GoTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            return this;
+        }
+
+        public Browser Back()
+        {
+            driver.Navigate().Back();
+            return this;
+        }
+
+        public Browser Forward()
+        {
+            driver.Navigate().Forward();
+            return this;
+        }
+
+        public Browser Click(string cssOrXpathSelector)
+        {
+            var elem = driver.FindElement(GetSelector(cssOrXpathSelector));
+
+            elem.Click();
+            return this;
+        }
+
+        private static By GetSelector(string cssOrXpathSelector)
+        {
+            if (cssOrXpathSelector.Contains("/") || cssOrXpathSelector.Contains("::"))
+                return By.XPath(cssOrXpathSelector);
+
+            return By.CssSelector(cssOrXpathSelector);
+        }
+
+        public Browser Set(string cssOrXpathSelector, string value)
+        {
+            var elem = driver.FindElement(GetSelector(cssOrXpathSelector));
+
+            elem.SendKeys(value);
+            return this;
+        }
+
+        public string Get(string cssOrXpathSelector)
+        {
+            var elem = driver.FindElement(GetSelector(cssOrXpathSelector));
+
+            if (elem.TagName.ToLowerInvariant() == "input" && elem.GetAttribute("type") == "text")
+                return elem.GetAttribute("value");
+
+            if (elem.TagName.ToLowerInvariant() == "select")
+                return elem.GetAttribute("value");
+
+            return elem.Text;
+        }
+
+        public string Title()
+        {
+            return driver.Title;
+        }
+
+        public Browser WaitFor(string cssOrXpathSelector)
+        {
+            WaitFor(cssOrXpathSelector, DefailtWaitInSeconds);
+            return this;
+        }
+
+        public Browser WaitFor(string cssOrXpathSelector, int seconds)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            var elem = wait.Until(d => d.FindElement(GetSelector(cssOrXpathSelector)));
+            return this;
+        }
+
+        public void Close()
+        {
+            driver.Close();
+            driver.Dispose();
+            driver = null;
+        }
+
+    }
+}
---
+++ StepOnThatTests/Browser/BrowserTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+
+namespace StepOnThat.Browser.Tests
+{
+    [TestFixture]
+    [Category("Browser")]
+    public class BrowserTests
+    {
+        private string homepageUrl = "http://www.bbc.co.uk";
+        private string searchboxCss = "input[type=text][name=q]";
+        [Test]
+        public void GoToTest()
+        {
+            var browser = Browser.Current;
+            browser.GoTo(homepageUrl);
+            Assert.IsTrue(browser.Title().Contains("BBC"));
+        }
+
+        [Test]
+        public void ClickAndWaitTest()
+        {
+            Browser.Current
+                .GoTo(homepageUrl)
+                .Click("a:link")
+                .WaitFor("img");
+            Assert.IsTrue(Browser.Current.Title().Contains("BBC"));
+        }
+
+        
+        [Test]
+        public void ClickUsingXPathTest()
+        {
+            Browser.Current
+                .GoTo(homepageUrl)
+                .Click("//*[@id='orb-nav-links']/ul/li[1]/a");
+
+            Assert.IsTrue(Browser.Current.Title().Contains("BBC News"));
+        }
+
+        [Test]
+        public void DoAndSubmitASearchTest()
+        {
+            var browser = Browser.Current;
+            browser.GoTo(homepageUrl);
+            browser.Set(searchboxCss, "news");
+            browser.Click(searchboxCss + " + input");
+            Assert.IsTrue(browser.Title().Contains("news"));
+        }
+
+        [Test]
+        public void GetTest()
+        {
+            var browser = Browser.Current;
+            var text = browser
+                .GoTo(homepageUrl)
+                .Get("nav a");
+            Assert.AreEqual("News", text);
+        }
+
+        [Test]
+        public void GetInputTest()
+        {
+            var browser = Browser.Current;
+            browser.GoTo(homepageUrl);
+            browser.Set(searchboxCss, "news");
+            var text = browser.Get(searchboxCss);
+            Assert.AreEqual("news", text);
+        }
+
+        [Test]
+        public void BackAndForwardTest()
+        {
+            Browser.Current
+                .GoTo(homepageUrl)
+                .Click("a:link")
+                .Back()
+                .Forward();
+            Assert.IsTrue(Browser.Current.Title().Contains("BBC"));
+        }
+
+        [TestFixtureTearDown]
+        public void AfterAlltests()
+        {
+            Browser.Current.Close();
+        }
+    }
+}
