56f936348537058c4e14b8c64363574dbab49823
Alastair Crabtree <alastaircrabtree@gmail.com>
2015-05-31 17:32:17 +0100

    Improve ToString on Property

--- src/StepOnThat/Property.cs
+++ src/StepOnThat/Property.cs
@@ -10,5 +10,10 @@
 
         public string Key { get; set; }
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}={1}", Key, Value);
+        }
     }
 }
